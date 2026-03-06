67a37aa4f3bf039bd72474502bb34878b9d164bc
Prince Raj <prince.raj160418@gmail.com>
2026-03-06 16:52:24 +0530

    MLflow Done

--- Network_Security/components/model_trainer.py
+++ Network_Security/components/model_trainer.py
@@ -24,6 +24,7 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
+import mlflow
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -33,7 +34,23 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    
+    def track_mlflow(self,best_model,classificationmetric):
+      mlflow.set_tracking_uri("file:./mlruns")
+      mlflow.set_experiment("network_security_project")
+
+      with mlflow.start_run():
+
+        f1_score=classificationmetric.f1_score
+        precision_score=classificationmetric.precision_score
+        recall_score=classificationmetric.recall_score
+
+        mlflow.log_metric("f1_score",f1_score)
+        mlflow.log_metric("precision",precision_score)
+        mlflow.log_metric("recall_score",recall_score)
+
+        mlflow.sklearn.log_model(best_model,"model")
+
+
         
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
@@ -85,11 +102,19 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+
+        #Track the MLflow
+        self.track_mlflow(best_model,classification_train_metric)
+        
         
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+
+        self.track_mlflow(best_model,classification_test_metric)
+
+        
 
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
