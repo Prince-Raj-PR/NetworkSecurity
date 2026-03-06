460f7236dd0a70d25aaac524097aa3c379b12e9f
Prince Raj <prince.raj160418@gmail.com>
2026-03-06 17:11:44 +0530

    Dagshub Repository and Final Models folder Created

--- Network_Security/components/model_trainer.py
+++ Network_Security/components/model_trainer.py
@@ -25,6 +25,9 @@ from sklearn.ensemble import (
 )
 
 import mlflow
+import dagshub
+dagshub.init(repo_owner='Prince_Raj_PR', repo_name='NetworkSecurity', mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -35,21 +38,18 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-      mlflow.set_tracking_uri("file:./mlruns")
-      mlflow.set_experiment("network_security_project")
+         
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
 
-      with mlflow.start_run():
+            
 
-        f1_score=classificationmetric.f1_score
-        precision_score=classificationmetric.precision_score
-        recall_score=classificationmetric.recall_score
-
-        mlflow.log_metric("f1_score",f1_score)
-        mlflow.log_metric("precision",precision_score)
-        mlflow.log_metric("recall_score",recall_score)
-
-        mlflow.sklearn.log_model(best_model,"model")
-
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
 
         
     def train_model(self,X_train,y_train,x_test,y_test):
